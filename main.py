28dce0fdecd3c86021ce010d6f735b9be6a6ce9c
binary-greens <arpanriju2000@gmail.com>
2019-10-21 21:40:43 +0530

    first

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 import sys
 
-def decimal_to_binary(dec): 
-    decimal = int(dec) 
-    print(decimal, " in Binary      : ", bin(decimal)) 
-   
-def decimal_to_octal(dec): 
-    decimal = int(dec)  
-    print(decimal, " in Octal       : ", oct(decimal)) 
-  
-def decimal_to_hexadecimal(dec): 
-    decimal = int(dec)  
-    print(decimal, " in Hexadecimal : ", hex(decimal)) 
 
-def binary_to_decimal(bi): 
-    print(bi, " in Decimal     : ", int(bi,2)) 
+def decimal_to_binary(dec):
+    decimal = int(dec)
+    print(decimal, " in Binary      : ", bin(decimal))
 
-def binary_to_octal(bi): 
-    print(bi, " in Octal       : ", oct(int(bi,2))) 
 
-def binary_to_hexadecimal(bi): 
-    print(bi, " in Binary      : ", hex(int(bi,2))) 
+def decimal_to_octal(dec):
+    decimal = int(dec)
+    print(decimal, " in Octal       : ", oct(decimal))
 
-if sys.argv[1]=='c':
-    if(sys.argv[2]=='d' or sys.argv[2]=='D'):
+
+def decimal_to_hexadecimal(dec):
+    decimal = int(dec)
+    print(decimal, " in Hexadecimal : ", hex(decimal))
+
+
+def binary_to_decimal(bi):
+    print(bi, " in Decimal     : ", int(bi, 2))
+
+
+def binary_to_octal(bi):
+    print(bi, " in Octal       : ", oct(int(bi, 2)))
+
+
+def binary_to_hexadecimal(bi):
+    print(bi, " in Binary      : ", hex(int(bi, 2)))
+
+
+if sys.argv[1] == 'c':
+    if sys.argv[2] == 'd' or sys.argv[2] == 'D':
         x = int(sys.argv[3])
         decimal_to_binary(x)
         decimal_to_hexadecimal(x)
         decimal_to_octal(x)
 
-    if(sys.argv[2]=='b' or sys.argv[2]=='B'):
+    if sys.argv[2] == 'b' or sys.argv[2] == 'B':
         x = sys.argv[3]
         binary_to_decimal(x)
         binary_to_hexadecimal(x)
